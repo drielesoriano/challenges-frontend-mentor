6313ece2a5177d64ed9e26c3274281cca8ab5170
drieleS <drielesoriano@gmail.com>
2020-08-27 22:19:59 -0300

    update project with html css and js modified

---
+++ article-preview-component-master/scripts.js
@@ -0,0 +1,6 @@
+function toggleShare() {
+  /* falta mudar a cor quando clicado*/
+
+   const socialsContainer = document.querySelector('.div-social-media')
+   socialsContainer.classList.toggle('show')
+}
